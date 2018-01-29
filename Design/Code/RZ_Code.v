9b0a4b4bd6c5d02d5ce733a7df95683b47ef2694
DOOKNET <zichen.ding@outlook.com>
2018-01-30 03:48:46 +0800

    版本1.0
    移位操作有点问题

--- Design/Code/RZ_Code.v
+++ Design/Code/RZ_Code.v
@@ -7,29 +7,31 @@ module RZ_Code(
 	input	clk,
 	input	rst_n,
 	input	[23:0]	RGB,	//按照GRB的顺序排列
+	input	done_sig,		//RGB数据更新信号
 	output	RZ_data
 );
 
 //-----------------接口信号----------------//
 reg 	[31:0]	cnt;
-reg		symbol_flag;		//码元周期标志,在每个码元的开始时刻
-reg 	[23:0]	RGB_reg;	//移位寄存器
-
-reg		data_out;		//转换后的单极性归零码
+reg		symbol;				//为了仿真好看
+reg 	[23:0]	RGB_shift;	//移位寄存器
+reg 	RGB_reg;			//存储移位寄存器最高位
+reg		data_out;			//转换后的单极性归零码
 //----------------------------------------//
 
 //-----------计数一个码元周期--------------//
 always @(posedge clk or negedge rst_n) begin
 	if(!rst_n)	begin
 		cnt <= 0;
+		symbol <= 0;
 	end
 	else if(cnt == 32'd59)	begin		//计数到60=1.2us * 50M
 		cnt <= 32'd0;
-		symbol_flag <= 1;
+		symbol <= 1;
 	end
 	else	begin
 		cnt <= cnt + 1'b1;
-		symbol_flag <= 0;
+		symbol <= 0;
 	end
 end
 //---------------------------------------//
@@ -37,12 +39,18 @@ end
 //--------------移位寄存器---------------//
 always @(posedge clk or negedge rst_n) begin
 	if(!rst_n)	begin
+		RGB_shift <= 0;
 		RGB_reg <= 0;
 	end
-	else if(symbol_flag == 1)	begin
-		RGB_reg <= (RGB_reg << 1);
+	else if(done_sig == 1)	begin
+		RGB_shift <= RGB;
+	end
+	else if(cnt == 32'd59)	begin	
+		RGB_shift <= (RGB_shift << 1);
+		RGB_reg <= RGB_shift[23];
 	end
 	else	begin
+		RGB_shift <= RGB_shift;
 		RGB_reg <= RGB_reg;
 	end
 end
@@ -53,20 +61,20 @@ always @(posedge clk or negedge rst_n) begin
 	if(!rst_n)	begin
 		data_out <= 0;
 	end
-	else if((symbol_flag == 1) && (RGB_reg[23] == 0))	begin
-		if(cnt == 32'd19)	begin		//零码，0.4us*50M=20
-			data_out <= 0;
+	else if(RGB_reg == 0)	begin
+		if(cnt <= 32'd19)	begin		//零码，0.4us*50M=20
+			data_out <= 1;
 		end
 		else	begin
-			data_out <= 1;
+			data_out <= 0;
 		end
 	end
-	else if((symbol_flag == 1) && (RGB_reg[23] == 1))	begin
-		if(cnt == 32'd39)	begin		//一码，0.8us*50M=40
-			data_out <= 0;
+	else if(RGB_reg == 1)	begin
+		if(cnt <= 32'd39)	begin		//一码，0.8us*50M=40
+			data_out <= 1;
 		end
 		else	begin
-			data_out <= 1;
+			data_out <= 0;
 		end
 	end
 	else	begin
--- TestBeach/tb_RZ.v
+++ TestBeach/tb_RZ.v
@@ -5,6 +5,7 @@ module	tb_RZ();
 reg 	sclk;
 reg 	rst_n;
 reg 	[23:0]	RGB;
+reg 	done_sig;
 wire 	RZ_Code;
 
 initial	sclk = 1;
@@ -12,17 +13,26 @@ always	#10	sclk = ~sclk;
 
 initial	begin
 	rst_n = 0;
+	RGB = 0;
+	done_sig = 0;
 	#100
 	rst_n = 1;
+	done_sig = 1;
 	RGB = 23'b10111001_11100100_00001110;
-	#500
+	#20
+	done_sig = 0;
+	#100000
+	done_sig = 1;
 	RGB = 23'b00001110_10111001_11100100;
+	#20
+	done_sig = 0;
 end
 
 RZ_Code			RZ_Code_inst(
 	.clk		(sclk),
 	.rst_n		(rst_n),
 	.RGB		(RGB),
+	.done_sig	(done_sig),
 	.RZ_data	(RZ_Code)
 );
 
